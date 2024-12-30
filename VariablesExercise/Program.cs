d6e998c89f4c1bb79aa454a78aa8d0e8aaf25692
Vannye Lima <vannye_l@yahoo.com>
2024-12-29 19:55:40 -0500

    variableexercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Vannye";
+            int age = 41;
+            double avg = 98.7;
+            decimal bankAccount = 10000.00m;
+            char initial = 'V';
+            bool isMarried = false;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried} ");
+            
+            
+
         }
     }
 }
